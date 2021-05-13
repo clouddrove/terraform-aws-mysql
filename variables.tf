ab7b5bec7d03b76ee6d0dc267dac984937f0bf5c
Sohan <sohan@clouddrove.com>
2021-05-13 17:15:30 +0530

    enabled encryption

--- variables.tf
+++ variables.tf
@@ -204,7 +204,7 @@ variable "publicly_accessible" {
 
 variable "monitoring_interval" {
   type        = number
-  default     = 0
+  default     = 1
   description = "The interval, in seconds, between points when Enhanced Monitoring metrics are collected for the DB instance. To disable collecting Enhanced Monitoring metrics, specify 0. The default is 0. Valid Values: 0, 1, 5, 10, 15, 30, 60."
 }
 
