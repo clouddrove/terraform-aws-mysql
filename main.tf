6d22a565b178c7e64ce695f0162b4b5e77ba62b2
theprashantyadav <theprashantyadav7@gmail.com>
2023-07-31 16:29:00 +0530

    feat: Added new vpc tag

--- main.tf
+++ main.tf
@@ -22,9 +22,9 @@ resource "random_id" "password" {
 locals {
   monitoring_role_arn = var.enabled_monitoring_role ? aws_iam_role.enhanced_monitoring[0].arn : var.monitoring_role_arn
 
-  final_snapshot_identifier = var.skip_final_snapshot ? null : "${var.final_snapshot_identifier_prefix}-${var.identifier}-${try(random_id.snapshot_identifier[0].hex, "")}"
-  identifier        = var.use_identifier_prefix ? null : var.identifier
-  identifier_prefix = var.use_identifier_prefix ? "${var.identifier}-" : null
+  final_snapshot_identifier   = var.skip_final_snapshot ? null : "${var.final_snapshot_identifier_prefix}-${var.identifier}-${try(random_id.snapshot_identifier[0].hex, "")}"
+  identifier                  = var.use_identifier_prefix ? null : var.identifier
+  identifier_prefix           = var.use_identifier_prefix ? "${var.identifier}-" : null
   monitoring_role_name        = var.monitoring_role_use_name_prefix ? null : var.monitoring_role_name
   monitoring_role_name_prefix = var.monitoring_role_use_name_prefix ? "${var.monitoring_role_name}-" : null
   db_subnet_group_name        = var.enabled_db_subnet_group ? join("", aws_db_subnet_group.this.*.id) : var.db_subnet_group_name
@@ -34,7 +34,7 @@ locals {
   password       = var.password == "" ? join("", random_id.password.*.b64_url) : var.password
   engine         = var.replicate_source_db != null ? null : var.engine
   engine_version = var.replicate_source_db != null ? null : var.engine_version
-  name = var.use_name_prefix ? null : var.name
+  name           = var.use_name_prefix ? null : var.name
   //  name_prefix = var.use_name_prefix ? "${var.name}-" : null
   description = coalesce(var.option_group_description, format("%s option group", var.name))
 }
